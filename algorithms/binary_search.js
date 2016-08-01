6522c63a1a51b3f3e77f8be30a0bb3a02dd0f2eb
surya <iamssurya@gmail.com>
2016-08-02 00:13:13 +0530

    Binary search js implementation

--- algorithms/binary_search.js
+++ algorithms/binary_search.js
@@ -1,3 +1,11 @@
-var binarySearch = function(search_string,data){
-	
-}
+var binarySearch = function(search_string,data,low,high){
+	var mid = low + (high-low)/2;
+	if(data[mid]==search_string){
+		return mid;
+	}else if(data[mid] < search_string){
+		return binarySearch(search_string,data,mid+1,high);
+	}else{
+		return binarySearch(search_string,data,mid-1,high);
+	}
+	return -1;
+}	
